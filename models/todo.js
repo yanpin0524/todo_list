14b867b26f4eee02c97efbab94ee7754761b20b4
Leo <a131234568@gmail.com>
2022-02-28 19:07:52 +0800

    Add models and todoSchema

---
+++ models/todo.js
@@ -0,0 +1,12 @@
+const { model } = require('mongoose')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const todoSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  }
+})
+
+module.exports = mongoose.model('Todo', todoSchema) 
