be45c7883256e74248ba0ee7f7204b6f2d11d658
Leo <a131234568@gmail.com>
2022-02-28 20:08:55 +0800

    Add todo seeder

---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Todo = require('../todo')
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb is error!!')
+  console.log('【 資料庫發生錯誤!! 】')
+})
+
+db.once('open', () => {
+  console.log('mongodb is connected.')
+  console.log('【 資料庫已連接 】')
+
+  for (let i = 1; i <= 10; i++) {
+    Todo.create({ name: `item_${i}` })
+  }
+
+  console.log('done.')
+})
