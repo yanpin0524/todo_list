b062627f0afe87cf77f377c97bffda5ecfac1b56
Leo <a131234568@gmail.com>
2022-03-01 14:36:08 +0800

    modify index.hbs 2

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const Todo = require('./models/todo')
 
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -23,9 +24,12 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find()
+    .lean()
+    .then(todos => res.render('index',{ todos }))
+    .catch(error => console.error(error))
 })
 
 app.listen(port, () => {
-  console.log(`Your URL is : localhost:${port}`)
+  console.log(`【 Your URL is : localhost:${port} 】`)
 })
