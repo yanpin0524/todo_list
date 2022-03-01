5ea8ba9f2240f5d1dd722c9185f7beaba5bedbaa
Leo <a131234568@gmail.com>
2022-03-01 17:58:37 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
@@ -11,23 +12,35 @@ mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUn
 const db = mongoose.connection
 
 db.on('error', () => {
-  console.log('mongodb is error!!')
   console.log('【 資料庫發生錯誤!! 】')
 })
 
 db.once('open', () => {
-  console.log('mongodb is connected.')
   console.log('【 資料庫已連接 】')
 })
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
-    .then(todos => res.render('index',{ todos }))
+    .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  
+  return Todo.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
