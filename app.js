6d0ae449dec55b55c53472a88cc9fc76170f5c65
Leo <a131234568@gmail.com>
2022-03-01 21:06:14 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -71,6 +71,14 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`【 Your URL is : localhost:${port} 】`)
 })
